b0b33b2ddb418fb0f827121381670afa3aae5fd5
Alcaide F. Inocente <ialcaidef@hotmail.com>
2020-09-27 19:21:07 +0200

    Add project files.

---
+++ ContosoConf/scripts/pages/register.js
@@ -0,0 +1,33 @@
+﻿    // Get the registration <form> element from the DOM.
+    const form = document.getElementById("registration-form");
+    const submitButton = form.querySelector("button");
+    // Get the <input> elements from the DOM.
+    const passwordInput = document.getElementById("password");
+    const confirmPasswordInput = document.getElementById("confirm-password");
+
+    function checkPasswords() {
+        const passwordsMatch = passwordInput.value === confirmPasswordInput.value;
+        if (passwordsMatch) {
+            // Clear any previous error message.
+            confirmPasswordInput.setCustomValidity("");
+        } else {
+            // Setting this error message will prevent the form from being submitted.
+            confirmPasswordInput.setCustomValidity("Your passwords don't match. Please type the same password again.");
+        }
+    };
+
+    function addPasswordInputEventListeners() {
+        passwordInput.addEventListener("input", checkPasswords, false);
+        confirmPasswordInput.addEventListener("input", checkPasswords, false);
+    };
+
+    function formSubmissionAttempted() {
+        form.classList.add("submission-attempted");
+    };
+
+    function addSubmitClickEventListener() {
+        submitButton.addEventListener("click", formSubmissionAttempted, false);
+    };
+
+    addPasswordInputEventListeners();
+    addSubmitClickEventListener();
